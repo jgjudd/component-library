820a114bafb4dc036f5068fc575930a28548ff6d
Justin <jjudd10@ford.com>
2020-05-24 23:19:27 -0400

    added initial files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <main>Main</main>
+      <footer>FOOTER</footer>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const styles = styled.header`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+export const Header = () => {
+  return (
+    <styles>
+      <header>HEADER</header>
+    </styles>
+  )
+}
